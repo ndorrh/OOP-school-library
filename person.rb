4fa3bd8d1ddbab90ec53308f4e3e4af3655bb794
ndorrh <ndorrh@gmail.com>
2023-01-03 16:08:11 +0100

    Fix linter error

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
