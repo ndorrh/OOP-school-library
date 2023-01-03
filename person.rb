b1c790c40f3aed3c0a9b3d91361a7a6c9152b251
ndorrh <ndorrh@gmail.com>
2023-01-03 13:47:56 +0100

    Add person class to person.rb

---
+++ person.rb
@@ -0,0 +1,28 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+  def initialize(age, parent_permission = true, name = "Unknown")
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @id = Random.rand(1..1000)
+  end
+
+private
+  def is_of_age
+    if @age >= 18
+      return true
+    end
+  end
+
+public
+  def can_use_services
+    if is_of_age || @parent_permission
+      return true
+    end
+  end
+end
+
+person1 = Person.new(5, true, "james")
+
+puts person1.can_use_services
