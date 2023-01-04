1e636baee464a398f30e837b35c83cdccb7d9b72
ndorrh <ndorrh@gmail.com>
2023-01-04 06:18:02 +0100

    Fix linter errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,7 +3,7 @@ require_relative('./decorator')
 class TrimmerDecorator < Decorator
   def correct_name
     # TrimmerDecorator inherit the initialize method and correct_name method from Decorator class
-    #you can use super or @name_able.correct_name to acess the decorator class since it has been inherited  
+    # you can use super or @name_able.correct_name to acess the decorator class since it has been inherited
     if super.length <= 10 # is the same as @name_able.correct_name <= 10
       @name_able.correct_name
     else
