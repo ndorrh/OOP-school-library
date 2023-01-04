d57cb8d1fe6185e7ee2af5b332627aae1dc00dbd
ndorrh <ndorrh@gmail.com>
2023-01-04 06:15:48 +0100

    Add comment to code

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,6 +2,8 @@ require_relative('./decorator')
 
 class CapitalizeDecorator < Decorator
   def correct_name
-    super.capitalize
+    # TrimmerDecorator inherit the initialize method and correct_name method from Decorator class
+    #you can use super or @name_able.correct_name to acess the decorator class since it has been inherited 
+    super.capitalize # is thesame as @name_able.correct_name.capitalize
   end
 end
