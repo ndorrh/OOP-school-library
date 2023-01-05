23d02b48ffad9b85cf5a41b97b8306d77ba7cf2b
ndorrh <ndorrh@gmail.com>
2023-01-05 11:43:33 +0100

    Fix minor errors

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative('./person')
 require_relative('./classroom')
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -10,10 +10,11 @@ class Student < Person
 
   def add_classroom(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯(ツ)/¯'
   end
 end
+
